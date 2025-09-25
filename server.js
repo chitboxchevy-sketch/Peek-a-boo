03b1d963568409be9ffa6c34a0117ce2bba590bd
chitboxchevy-sketch <chitboxchevy@gmail.com>
2025-09-25 15:05:26 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('offer', (data) => {
+        socket.broadcast.emit('offer', data);
+    });
+
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+
+    socket.on('candidate', (data) => {
+        socket.broadcast.emit('candidate', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
